629a42cbb6936be1c9dedee73ce31ba33e36e3b4
Carlos Manuel Perez Carrizal <75050886+CarlitosPC2810@users.noreply.github.com>
2021-04-21 16:34:48 -0500

    agg :(

--- Controllers/userController.php
+++
@@ -1,4 +0,0 @@
-<?php 
-require_once "../models/adminModel.php";
-$data = json_decode(file_get_contents('php://input'), true);
-?>
--- Models/userModel.php
+++
@@ -1,6 +0,0 @@
-<?php   
-    require_once 'connectionModel.php';
-    class userModel{
-        
-    }
-?>
